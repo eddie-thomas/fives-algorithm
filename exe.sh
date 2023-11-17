e983b78f9df092b56d2009ca171774c69696c048
eddie-semarts <eddie.thomas@semanticarts.com>
2023-11-17 13:35:52 -0700

    Initial commit

---
+++ exe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source .venv/bin/activate && echo -e "Running algorithm...\n\n"
+
+python main.py
+
+deactivate
---
+++ main.py
@@ -0,0 +1,10 @@
+from words import ALL_WORDS
+
+
+def analyze_word():
+    """Iteratively analyze guesses"""
+    print("Test")
+
+
+if "__main__" == __name__:
+    analyze_word()
